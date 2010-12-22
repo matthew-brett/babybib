a25296176010ffc461498c404c6d15679c8b02f4
Matthew Brett <matthew.brett@gmail.com>
2010-12-22 18:48:01 +0000

    RF + TEST - refactoring and thinking about macros

--- babybib/bibparsers.py
+++ babybib/bibparsers.py
@@ -17,7 +17,7 @@ that BibTeX uses.
 
 Greg Ward also wrote a parser for BibTeX files, and wrote it up in detail, in
 the documentation for btparse and Text::BibTeX.  His grammar is like that of
-bibparse in that it enforces slightly tighter rules than BibTeX itself.
+bibparse in that it enforces slightly tighter rules than BibTeX it
 
 .. _Nelson Beebe's bibliography tools: http://www.math.utah.edu/~beebe/software/bibtex-bibliography-tools.html
 .. _btparse: http://search.cpan.org/dist/btparse
@@ -26,67 +26,54 @@ Matthew Brett 2010
 Simplified BSD license
 """
 
-DEBUG = True
-
 import re
 
 from pyparsing import (Regex, Suppress, ZeroOrMore, Group, Optional, Forward,
                        Literal, SkipTo, lineStart, Word, nums)
 
-# Our character definitions
-chars_no_curly = Regex(r"[^{}]+")
-chars_no_curly.leaveWhitespace()
-chars_no_quotecurly = Regex(r'[^"{}]+')
-chars_no_quotecurly.leaveWhitespace()
-lcurly = Suppress('{')
-rcurly = Suppress('}')
-lparen = Suppress('(')
-rparen = Suppress(')')
-quote = Suppress('"')
-comma = Suppress(',')
-at = Suppress('@')
-equals = Suppress('=')
-hash = Literal('#')
 
-# Define parser for strings (the hard bit)
+# Our character definitions
+CHARS_NO_CURLY = Regex(r"[^{}]+")
+CHARS_NO_CURLY.leaveWhitespace()
+CHARS_NO_QUOTECURLY = Regex(r'[^"{}]+')
+CHARS_NO_QUOTECURLY.leaveWhitespace()
+LCURLY = Suppress('{')
+RCURLY = Suppress('}')
+LPAREN = Suppress('(')
+RPAREN = Suppress(')')
+QUOTE = Suppress('"')
+COMMA = Suppress(',')
+AT = Suppress('@')
+EQUALS = Suppress('=')
+HASH = Suppress('#')
+
+
+def bracketed(expr):
+    """ Return matcher for `expr` between curly brackets or parentheses """
+    return (LPAREN + expr + RPAREN) | (LCURLY + expr + RCURLY)
+
+
+# Define parser components for strings (the hard bit)
 # Curly string is some stuff without curlies, or nested curly sequences
 curly_string = Forward()
-curly_item = Group(curly_string) | chars_no_curly
-curly_string << lcurly + ZeroOrMore(curly_item) + rcurly
+curly_item = Group(curly_string) | CHARS_NO_CURLY
+curly_string << LCURLY + ZeroOrMore(curly_item) + RCURLY
 # quoted string is either just stuff within quotes, or stuff within quotes, within
 # which there is nested curliness
-quoted_item = Group(curly_string) | chars_no_quotecurly
-quoted_string = quote + ZeroOrMore(quoted_item) + quote
+quoted_item = Group(curly_string) | CHARS_NO_QUOTECURLY
+quoted_string = QUOTE + ZeroOrMore(quoted_item) + QUOTE
 
-# Now we define the entry contents.  The following list of characters is from
-# the btparse documentation
-any_name = Regex(r'[^\s"#%\'(),={}]+')
+# Numbers can just be numbers. Only integers though.
+number = Word(nums)
+
+# Basis characters (by exclusion) for variable / field names.  The following
+# list of characters is from the btparse documentation
+any_name = Regex('[^\s"#%\'(),={}]+')
+
 # btparse says, and the test bibs show by experiment, that macro and field names
 # cannot start with a digit.  In fact entry type names cannot start with a digit
 # either (see tests/bibs). Cite keys can start with a digit
-not_digname = Regex(r'[^\d\s"#%\'(),={}][^\s"#%\'(),={}]*')
-# macro names appearing on RHS and LHS of expressions have different parse
-# actions.  Both are converted to strings. RHS occurences are a) checked for
-# being in the parse dictionary, warning if not (null contents).  LHS occurences
-# go into the the parse dictionary.
-macro_lhs = not_digname.copy()
-macro_rhs = not_digname.copy()
-field_name = not_digname.copy()
-entry_type = not_digname.copy()
-cite_key = any_name.copy()
-# Numbers can just be numbers. Only integers though.
-number = Word(nums)
-# Number has to be before macro name
-string = number | macro_rhs | quoted_string | curly_string
-
-# There can be hash concatenation
-field_value = string + ZeroOrMore(hash + string)
-field_def = field_name + equals + field_value
-entry_contents = Group(ZeroOrMore(field_def + comma) + Optional(field_def))
-
-# Convenience for our two types of brackets
-def bracketed(expr):
-    return (lparen + expr + rparen) | (lcurly + expr + rcurly)
+not_digname = Regex('[^\d\s"#%\'(),={}][^\s"#%\'(),={}]*')
 
 # where to go if there's an error during processing
 error_start = Suppress(SkipTo(lineStart + '@'))
@@ -94,28 +81,67 @@ error_start = Suppress(SkipTo(lineStart + '@'))
 # Where to go when we've finished with the last entry
 normal_start = Suppress(SkipTo('@'))
 
+# Comment comments out to end of line
+comment = AT + Regex('comment.*', re.IGNORECASE)
+
+# Preamble start
+preamble_id = Regex('preamble', re.IGNORECASE)
+
+# Macro (string) start
+macro_id = Regex('string', re.IGNORECASE)
+
+# Id for macro def
+class Macro(object):
+    def __init__(self, name):
+        self.name = name
+    def __repr__(self):
+        return 'Macro("%s")' % self.name
+    def __eq__(self, other):
+        return self.name == other.name
+    def __ne__(self, other):
+        return self.name != other.name
+
+# The name types with their digiteyness
+macro_def = not_digname.copy()
+macro_ref = not_digname.copy().setParseAction(lambda s,l,t : Macro(t[0]))
+field_name = not_digname.copy()
+entry_type = not_digname.setResultsName('entry_type')
+cite_key = any_name.setResultsName('cite_key')
+# Number has to be before macro name
+string = (number | macro_ref | quoted_string |
+          curly_string)
+
+# There can be hash concatenation
+field_value = string + ZeroOrMore(HASH + string)
+field_def = field_name + EQUALS + field_value
+entry_contents = Group(
+    ZeroOrMore(field_def + COMMA) + Optional(field_def))
+
 # Entry is surrounded either by parentheses or curlies
-entry = (at + entry_type + bracketed(cite_key + comma + entry_contents) |
+entry = (AT + entry_type +
+         bracketed(cite_key + COMMA + entry_contents) |
          error_start)
 
-# Comment comments out to end of line
-comment = at + Regex('comment.*', re.IGNORECASE)
-
 # Preamble is a macro-like thing with no name
-preamble_id = Regex('preamble', re.IGNORECASE)
-preamble = at + preamble_id + bracketed(field_value) | error_start
+preamble = (AT + preamble_id +
+            bracketed(field_value) | error_start)
 
 # Macros (aka strings)
-macro_id = Regex('string', re.IGNORECASE)
-macro = at + macro_id + bracketed(macro_lhs + equals + field_value) | error_start
+macro = (AT + macro_id +
+         bracketed(macro_def + EQUALS + field_value)
+         | error_start)
 
 # entries are last in the list (other than the fallback) because they have
-# arbitrary start patterns that would match commments, preamble or macro
-definitions = comment | preamble | macro | entry | normal_start
+# arbitrary start patterns that would match comments, preamble or macro
+definitions = (comment |
+               preamble |
+               macro |
+               entry |
+               normal_start)
 
 # Start symbol
 bibfile = ZeroOrMore(definitions)
 
 
-def parse_str(bib_str):
-    return bibfile.parseString(bib_str)
+def parse_str(str):
+    return bibfile.parseString(str)
--- babybib/tests/test_bibparse.py
+++ babybib/tests/test_bibparse.py
@@ -2,34 +2,56 @@
 
 from pyparsing import ParseException, OneOrMore
 from .. import bibparsers as bp
+from ..bibparsers import Macro
 
 from nose.tools import assert_true, assert_false, assert_equal, assert_raises
 
 
 def test_names():
-    # check names
-    citation_key, field_name, macro_name = (
-        bp.citation_key, bp.field_name, bp.macro_name)
-    # key names can start with a digit
-    assert_equal()
-    assert_raises(ParseException, bp.name.parseString, '## ')
-    name_checker = OneOrMore(bp.field_name)
-    assert_equal(name_checker.parseString('a name or two').asList(),
-                 ['a','name','or','two'])
-    # check that names also work for strings
-    assert_equal(bp.string.parseString('someascii')[0], 'someascii')
-    assert_raises(ParseException, bp.string.parseString, '%#= validstring')
+    # check various types of names
+    # All names can contains alphas, but not some special chars
+    bad_chars = '"#%\'(),={}'
+    for name_type, dig1f in ((bp.macro_def, False),
+                             (bp.field_name, False),
+                             (bp.entry_type, False),
+                             (bp.cite_key, True)):
+        if dig1f: # can start with digit
+            assert_equal(name_type.parseString('2t')[0], '2t')
+        else:
+            assert_raises(ParseException, name_type.parseString, '2t')
+        # All of the names cannot contain some characters
+        for char in bad_chars:
+            assert_raises(ParseException, name_type.parseString, char)
+        # standard strings all OK
+        assert_equal(name_type.parseString('simple_test')[0], 'simple_test')
+    # Test macro ref
+    mr = bp.macro_ref
+    # can't start with digit
+    assert_raises(ParseException, mr.parseString, '2t')
+    for char in bad_chars:
+        assert_raises(ParseException, mr.parseString, char)
+    assert_equal(mr.parseString('simple_test')[0].name, 'simple_test')
+
+
+def test_numbers():
+    assert_equal(bp.number.parseString('1066')[0], '1066')
+    assert_equal(bp.number.parseString('0')[0], '0')
+    assert_raises(ParseException, bp.number.parseString, '-4')
+    assert_raises(ParseException, bp.number.parseString, '+4')
+    assert_raises(ParseException, bp.number.parseString, '.4')
+    # something point something leaves a trailing .4 unmatched
+    assert_equal(bp.number.parseString('0.4')[0], '0')
 
 
 def test_parse_string():
     # test string building blocks
-    assert_equal(bp.chars_no_quotecurly.parseString('x')[0], 'x')
-    assert_equal(bp.chars_no_quotecurly.parseString("a string")[0], 'a string')
-    assert_equal(bp.chars_no_quotecurly.parseString('a "string')[0], 'a ')
-    assert_equal(bp.chars_no_curly.parseString('x')[0], 'x')
-    assert_equal(bp.chars_no_curly.parseString("a string")[0], 'a string')
-    assert_equal(bp.chars_no_curly.parseString('a {string')[0], 'a ')
-    assert_equal(bp.chars_no_curly.parseString('a }string')[0], 'a ')
+    assert_equal(bp.CHARS_NO_QUOTECURLY.parseString('x')[0], 'x')
+    assert_equal(bp.CHARS_NO_QUOTECURLY.parseString("a string")[0], 'a string')
+    assert_equal(bp.CHARS_NO_QUOTECURLY.parseString('a "string')[0], 'a ')
+    assert_equal(bp.CHARS_NO_CURLY.parseString('x')[0], 'x')
+    assert_equal(bp.CHARS_NO_CURLY.parseString("a string")[0], 'a string')
+    assert_equal(bp.CHARS_NO_CURLY.parseString('a {string')[0], 'a ')
+    assert_equal(bp.CHARS_NO_CURLY.parseString('a }string')[0], 'a ')
     # test more general strings together
     for obj in (bp.curly_string, bp.string, bp.field_value):
         assert_equal(obj.parseString('{}').asList(), [])
@@ -45,19 +67,16 @@ def test_parse_string():
                     ['a ', ['nested'], ' string'])
         assert_equal(obj.parseString('"a {double {nested}} string"').asList(),
                     ['a ', ['double ', ['nested']], ' string'])
-    # check names
-    assert_raises(ParseException, bp.name.parseString, '## ')
-    name_checker = OneOrMore(bp.name)
-    assert_equal(name_checker.parseString('a name or two').asList(),
-                 ['a','name','or','two'])
-    # check that names also work for strings
-    assert_equal(bp.string.parseString('someascii')[0], 'someascii')
+    # check macro def in string
+    assert_equal(bp.string.parseString('someascii')[0], Macro('someascii'))
     assert_raises(ParseException, bp.string.parseString, '%#= validstring')
 
 
 def test_parse_field():
     # test field value - hashes included
     fv = bp.field_value
-    assert_equal(fv.parseString('aname')[0], 'aname')
-
-
+    assert_equal(fv.parseString('aname')[0], Macro('aname'))
+    assert_equal(fv.parseString('aname # "some string"').asList(),
+                 [Macro('aname'), 'some string'])
+    assert_equal(fv.parseString('aname # {some {string}}').asList(),
+                 [Macro('aname'), 'some ', ['string']])
